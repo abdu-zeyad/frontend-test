f28dab856043a54537ba7bf245fc38654f42f359
abdu-zeyad <abd.d@live.com>
2021-07-06 12:04:31 +0300

    addtofav

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,47 @@
 import React, { Component } from "react";
-
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Row, Col } from "react-bootstrap";
+import Item from "./Item";
 export class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+    };
+  }
   componentDidMount = async () => {
     const server = process.env.REACT_APP_SERVER;
-    console.log(server);
+    const allData = await axios.get(`${server}/all`);
+    this.setState({
+      data: allData.data,
+    });
+
+    console.log(this.state.data);
+  };
+  AddToFav = async (idx) => {
+    const server = process.env.REACT_APP_SERVER;
+    const obj = {
+      strDrink: this.state.data[idx].strDrink,
+      strDrinkThumb: this.state.data[idx].strDrinkThumb,
+    };
+    await axios.post(`${server}/addtofav`);
+    console.log(obj);
   };
   render() {
-    return <div>home</div>;
+    return (
+      <div>
+        <Row xs={1} md={3} className="g-4">
+          {this.state.data.map((item, idx) => {
+            return (
+              <Col>
+                <Item item={item} idx={idx} AddToFav={this.AddToFav} />
+              </Col>
+            );
+          })}
+        </Row>
+      </div>
+    );
   }
 }
 
